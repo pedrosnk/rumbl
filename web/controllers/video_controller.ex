484e7b7aace12b584ad8fa6c9994d1f1acbdd3bc
Pedro Medeiros <pedrosnk@gmail.com>
2015-11-08 03:08:21 -0200

    Add categories to videos

--- web/controllers/video_controller.ex
+++ web/controllers/video_controller.ex
@@ -4,6 +4,7 @@ defmodule Rumbl.VideoController do
   alias Rumbl.Video
 
   plug :scrub_params, "video" when action in [:create, :update]
+  plug :load_categories when action in [:new, :create, :edit, :update]
 
   def action conn, _ do
     apply(__MODULE__, action_name(conn), 
@@ -78,5 +79,13 @@ defmodule Rumbl.VideoController do
 
   defp user_videos user do
     assoc user, :videos
+  end
+
+  defp load_categories conn, _ do
+    categories = 
+      Repo.all from c in Rumbl.Category,
+                order_by: c.name,
+                select: {c.name, c.id}
+    assign conn, :categories, categories
   end
 end
--- web/router.ex
+++ web/router.ex
@@ -18,6 +18,7 @@ defmodule Rumbl.Router do
     pipe_through :browser # Use the default browser stack
 
     resources "/users", UserController
+    resources "/videos", VideoController
     resources "/sessions", SessionController, only: [:new, :create, :delete]
     
     get "/", PageController, :index
