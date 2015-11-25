33c03b1acbc5d94a0ef612c6a0d8c4e585353dbe
Pedro Medeiros <pedrosnk@gmail.com>
2015-11-25 05:59:56 -0200

    refactor user channel

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -3,11 +3,12 @@
 
 // To use Phoenix channels, the first step is to import Socket
 // and connect at the socket path in "lib/my_app/endpoint.ex":
+
 import {Socket} from "deps/phoenix/web/static/js/phoenix"
 
 let socket = new Socket("/socket", {
-	params: {token: window.userToken},
-	logger: (kind, msg, data) => { console.log(`${kind}: ${msg}`, data) }
+  params: {token: window.userToken},
+  logger: (kind, msg, data) => { console.log(`${kind}: ${msg}`, data) }
 })
 
 export default socket
