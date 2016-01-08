02c981d8c01353eb27ca1d66aeea2f8ef59cc92f
Pedro Medeiros <pedrosnk@gmail.com>
2016-01-08 07:40:00 -0200

    upgrade t phoenix 1.1

--- web/controllers/video_controller.ex
+++ web/controllers/video_controller.ex
@@ -19,7 +19,7 @@ defmodule Rumbl.VideoController do
   def new(conn, _params, user) do
     changeset =
       user
-      |> build(:videos)
+      |> build_assoc(:videos)
       |> Video.changeset()
     render(conn, "new.html", changeset: changeset)
   end
@@ -27,7 +27,7 @@ defmodule Rumbl.VideoController do
   def create(conn, %{"video" => video_params}, user) do
     changeset = 
       user
-      |> build(:videos)
+      |> build_assoc(:videos)
       |> Video.changeset(video_params)
 
     case Repo.insert(changeset) do
